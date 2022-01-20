6a27ae4f018bdaf1e49070c5f14bc6e225025d8b
Trane <vanjba@bk.ru>
2022-01-20 19:51:36 +0700

    edit: black

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('homepage.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("homepage.urls")),
 ]
 
 if settings.DEBUG:
